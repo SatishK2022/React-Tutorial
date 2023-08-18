1c4f34b56a1f5ad9125180fd9d64eb94c59024d9
SatishK2022 <sk7399052@gmail.com>
2023-08-18 17:45:06 +0530

    Learnt Components

---
+++ Day-2/first-app/Component/Test.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// React Element
+const title = (
+    <div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, suscipit!</p>
+    </div>
+);
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Hello React Namaste</h1>
+      <h2>Satish Kumar</h2>
+      {title}
+    </div>
+  );
+};
+
+export default Test;
