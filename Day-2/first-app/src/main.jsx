47d8149ea87fc2e827b2a86ee22d54fbdc3f2ddc
SatishK2022 <sk7399052@gmail.com>
2023-08-20 21:36:28 +0530

    Food Villa Project

--- Day-2/first-app/src/main.jsx
+++ Day-2/first-app/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Test from '../Component/Test.jsx'
+import AppLayout from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <Test />
+    <AppLayout />
   </React.StrictMode>,
 )
