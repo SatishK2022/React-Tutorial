f6b2cb8db57e560a51a0a4b9fb76d8f3dabf239b
SatishK2022 <sk7399052@gmail.com>
2023-08-22 16:45:23 +0530

    Added search Feature

--- Day-2/first-app/src/components/Body.jsx
+++ Day-2/first-app/src/components/Body.jsx
@@ -2,9 +2,18 @@ import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants";
 import { useState } from "react";
 
+function filterData(searchTxt, restaurants) {
+  const filterData =  restaurants.filter((restaurant) =>
+    restaurant.info.name.includes(searchTxt)
+  );
+
+  return filterData;
+}
+
 const Body = () => {
-  const [searchTxt, setSearchTxt] = useState("KFC");
-  
+  const [searchTxt, setSearchTxt] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
   return (
     <div className="body">
       <div className="search-container">
@@ -14,16 +23,27 @@ const Body = () => {
           placeholder="Search..."
           value={searchTxt}
           onChange={(e) => {
-            setSearchTxt(e.target.value)
+            const searchText = e.target.value;
+            setSearchTxt(searchText);
+            if (searchText === "") {
+              setRestaurants(restaurantList);
+            }
           }}
         />
-        <button>Search</button>
+        <button
+          onClick={() => {
+            const data = filterData(searchTxt, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
 
       <div className="products">
         {/* <h1 className="card-heading">Restaurants</h1> */}
         <div className="card-container">
-          {restaurantList.map((restaurant) => {
+          {restaurants.map((restaurant) => {
             return (
               <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
             );
